dca53a5a842e254fd98670a26b6ed8e66ada7044
Cem GUNGOR <51783087+Bratox@users.noreply.github.com>
2021-05-16 17:03:57 +0200

    Main and requirements

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from controller.player_controller import PlayerController
 from model.database import DatabaseEngine
 from PySide6.QtWidgets import QApplication
 
-
-
 from exceptions import Error
-from model.dao.player_dao import PlayerDAO
 
 if __name__ == '__main__':
     database_engine = DatabaseEngine(url='sqlite:///embuscade.db')
