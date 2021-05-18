e22e6d9ef83f5788a566a7279a22401e80237894
FlexReflx <leo.bendref@gmail.com>
2021-05-18 19:14:47 +0200

    lecture fichiers + instanciations des question => main

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ from controller.embuscade_controller import EmbuscadeController
 from model.database import DatabaseEngine
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from vue.embuscade import Ui_MainWindow as MainWindow
-
+import secrets
 from exceptions import Error
 
 def run():
     database_engine = DatabaseEngine(url='sqlite:///embuscade.db')
     database_engine.create_database()
+
     admin_controller = PlayerController(database_engine)
     question_controller = QuestionController(database_engine)
     jeu_controller = jeuController(database_engine)
     act_ver_controller = Act_verController(database_engine)
     virus_controller = VirusController(database_engine)
     embuscade_controller = EmbuscadeController(database_engine)
+
     question_file = open("question.txt", "r")
     virus_file = open("virus.txt", "r")
     embuscade_file = open("embuscade.txt", "r")
@@ -28,15 +30,18 @@ def run():
     jeu_file = open("jeu.txt", "r")
 
     for question in question_file.readlines():
-        question_controller.create_question(question)
+        if(question[0] == "Qui"):
+            question_controller.create_question(question+ " \n\n l'heureux.se élu.e se gratifié.e de "+ str(secrets.randbelow(3)+2)+" gorgées.")
+        else :
+            question_controller.create_question(question + "\n\n Si la réponse n'est pas bonne, l'inculte prendra "+ str(secrets.randbelow(3)+2)+" gorgées.")
     for virus in virus_file.readlines():
-        virus_controller.create_virus(virus)
+        virus_controller.create_virus(virus + "\n\n Tout manquement à ce virus sera puni de "+ str(secrets.randbelow(4)+3)+"\n\n Ce virus dure " + str(secrets.randbelow(2)+1) + " tours")
     for act_ver in act_ver_file.readlines():
         act_ver_controller.create_act_ver(act_ver)
     for embuscade in embuscade_file.readlines():
         embuscade_controller.create_embuscade(embuscade)
     for jeu in jeu_file.readlines():
-        jeu_controller.create_jeu(jeu)
+        jeu_controller.create_jeu(jeu + "Le ou les perdants.es boivent "+str(secrets.randbelow(3)+2)+"gorgées.")
 
     app = QApplication(sys.argv)
 
