9e60a5ed3d022c181c5f493a218ab0fb2e731bce
Cem GUNGOR <51783087+Bratox@users.noreply.github.com>
2021-05-18 22:09:33 +0200

    ChangeSoftAndSomeFunc

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from controller.virus_controller import VirusController
 from controller.jeu_controller import jeuController
 from controller.act_ver_controllers import Act_verController
 from controller.embuscade_controller import EmbuscadeController
+from controller.game_controller import GameController
 from model.database import DatabaseEngine
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from vue.embuscade import Ui_MainWindow as MainWindow
@@ -15,13 +16,9 @@ from exceptions import Error
 def run():
     database_engine = DatabaseEngine(url='sqlite:///embuscade.db')
     database_engine.create_database()
-    admin_controller = PlayerController(database_engine)
-    question_controller = QuestionController(database_engine)
-    jeu_controller = jeuController(database_engine)
-    act_ver_controller = Act_verController(database_engine)
-    virus_controller = VirusController(database_engine)
-    embuscade_controller = EmbuscadeController(database_engine)
-    question_file = open("question.txt", "r")
+    player_controller = PlayerController(database_engine)
+
+    """question_file = open("question.txt", "r")
     virus_file = open("virus.txt", "r")
     embuscade_file = open("embuscade.txt", "r")
     act_ver_file = open("act_ver.txt", "r")
@@ -36,12 +33,12 @@ def run():
     for embuscade in embuscade_file.readlines():
         embuscade_controller.create_embuscade(embuscade)
     for jeu in jeu_file.readlines():
-        jeu_controller.create_jeu(jeu)
+        jeu_controller.create_jeu(jeu)"""
 
     app = QApplication(sys.argv)
 
     MWind = QMainWindow()
-    ui = MainWindow(admin_controller, MWind)
+    ui = MainWindow(player_controller, database_engine, MWind)
     sys.exit(app.exec_())
 
 
