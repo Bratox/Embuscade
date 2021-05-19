b6b5ab6782312994a1d29d706ebe1f7e808ee4d8
FlexReflx <leo.bendref@gmail.com>
2021-05-19 20:37:56 +0200

    main changes insert data

--- main.py
+++ main.py
@@ -29,16 +29,16 @@ def run():
     act_ver_file = open("act_ver.txt", "r")
     jeu_file = open("jeu.txt", "r")
 
-    """for t in question_controller.list_question():
-        print(t)"""
+    for t in question_controller.list_question():
+        print(t)
     for question in question_file.readlines():
         print(question)
         if(question.split(" ")[0] == "Qui"):
             question_controller.create_question({'exp':question+ " \n\n l'heureux.se élu.e se gratifié.e de "+ str(secrets.randbelow(3)+2)+" gorgées."})
         else :
             question_controller.create_question({ 'exp':question + "\n\n Si la réponse n'est pas bonne, l'inculte prendra "+ str(secrets.randbelow(3)+2)+" gorgées."})
-
-    """for virus in virus_file.readlines():
+    """
+    for virus in virus_file.readlines():
         virus_controller.create_virus({'exp' :virus + "\n\n Tout manquement à ce virus sera puni de "+ str(secrets.randbelow(4)+3)+"\n\n Ce virus dure " + str(secrets.randbelow(2)+1) + " tours"})
     for act_ver in act_ver_file.readlines():
         act_ver_controller.create_act_ver({'exp':act_ver})
