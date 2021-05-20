e63dd1523391ed8ba3421fea68b2262044410681
FlexReflx <leo.bendref@gmail.com>
2021-05-20 15:48:10 +0200

    main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+import secrets
 from controller.player_controller import PlayerController
 from controller.question_controller import QuestionController
 from controller.virus_controller import VirusController
@@ -19,11 +19,17 @@ def run():
     database_engine.create_database()
     player_controller = PlayerController(database_engine)
 
-    """question_file = open("question.txt", "r")
+    question_file = open("question.txt", "r")
     virus_file = open("virus.txt", "r")
     embuscade_file = open("embuscade.txt", "r")
     act_ver_file = open("act_ver.txt", "r")
     jeu_file = open("jeu.txt", "r")
+
+    question_controller = QuestionController(database_engine)
+    virus_controller = VirusController(database_engine)
+    act_ver_controller = Act_verController(database_engine)
+    embuscade_controller = EmbuscadeController(database_engine)
+    jeu_controller = jeuController(database_engine)
 
     for t in question_controller.list_question():
         print(t)
@@ -35,16 +41,12 @@ def run():
             question_controller.create_question({ 'exp':question + "\n\n Si la réponse n'est pas bonne, l'inculte prendra "+ str(secrets.randbelow(3)+2)+" gorgées."})
     for virus in virus_file.readlines():
         virus_controller.create_virus({'exp' :virus + "\n\n Tout manquement à ce virus sera puni de "+ str(secrets.randbelow(4)+3)+"\n\n Ce virus dure " + str(secrets.randbelow(2)+1) + " tours"})
-    for question in question_file.readlines():
-        question_controller.create_question(question)
-    for virus in virus_file.readlines():
-        virus_controller.create_virus(virus)
     for act_ver in act_ver_file.readlines():
         act_ver_controller.create_act_ver(act_ver)
     for embuscade in embuscade_file.readlines():
         embuscade_controller.create_embuscade(embuscade)
     for jeu in jeu_file.readlines():
-        jeu_controller.create_jeu(jeu)"""
+        jeu_controller.create_jeu(jeu)
 
     app = QApplication(sys.argv)
 
