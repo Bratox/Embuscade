36ea8782588152e357a4e32b0f60253d5606e438
Cem GUNGOR <51783087+Bratox@users.noreply.github.com>
2021-05-20 15:50:20 +0200

    PyQT Init (ง︡'-'︠)ง

--- vue/__init__.py
+++ vue/__init__.py
@@ -1,2 +1,2 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
-from PySide6.QtGui import QCloseEvent
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
+from PyQt5.QtGui import QCloseEvent
